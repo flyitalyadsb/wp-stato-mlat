1ad1f7bf63fc3671b51b2bbe55098972db98cce0
simonedegiacomi <simonedegiacomi97@gmail.com>
2021-01-26 17:21:18 +0100

    crea file plugin

---
+++ wp-mlat-sync.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: wp-mlat-sync
+ */
+
+add_shortcode(' wp-mlat-sync-table', 'render_table');
+
+function render_table($atts = [], $content = null) {
+    return "Hello world";
+}
+
